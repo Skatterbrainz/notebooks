b3cdcf1bc3129b71694a741fbdcda64c5a596760
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2023-06-12 18:22:01 -0400

    removed azuread module depenendency. bug fixes

--- ps-graph/graph-users.ps1
+++
@@ -1,9 +0,0 @@
-Import-Module Microsoft.Graph.Users
-
-if (!$conn) { $global:conn = Connect-MgGraph User.Read.All,Organization.Read.All,Directory.Read.All }
-
-$users = Get-MgUser -All -Property Id,DisplayName,Mail,UserPrincipalName,UsageLocation,UserType,LicenseAssignmentStatus,AccountEnabled,Title
-
-$users.Count
-
-$users | Select AccountEnabled,DisplayName,UserPrincipalName,UserType,UsageLocation | FT
